9fe02bcf0b4fa67fa8ee1ee1c70d372680483b4a
Miguel Useche <miguel.useche@skillshare.com>
2025-11-23 13:12:52 -0500

    Add ESLint setup with vue-eslint-parser v9

---
+++ apps/vue/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2022: true,
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+  ],
+  rules: {
+    // Customize linting rules here if needed
+  },
+};
