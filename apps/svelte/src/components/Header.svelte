1728187b5427cdfb754a0445701737626a32f8e0
Miguel Useche <miguel.useche@skillshare.com>
2025-11-25 16:41:49 -0500

    Align header styling across apps

--- apps/svelte/src/components/Header.svelte
+++ apps/svelte/src/components/Header.svelte
@@ -1,22 +1,29 @@
 <script>
   import { derived } from 'svelte/store'
+  import { location } from 'svelte-spa-router'
   import { authStore } from '../stores/auth'
 
   const username = derived(authStore, ($auth) => $auth.user?.username)
 </script>
 
 <header class="navbar">
-  <div class="container" style="display: flex; align-items: center; justify-content: space-between;">
-    <a class="logo" href="/">conduit</a>
-    <nav class="nav-links">
-      <a href="/">Home</a>
+  <div class="container navbar-content">
+    <a class="navbar-logo" href="/">conduit</a>
+    <nav class="navbar-links">
+      <a class="navbar-link" class:active={$location === '/'} href="/">Home</a>
       {#if $username}
-        <a href="/editor">New Article</a>
-        <a href="/settings">Settings</a>
-        <a href={`/profile/${$username}`}>{$username}</a>
+        <a class="navbar-link" class:active={$location.startsWith('/editor')} href="/editor">New Article</a>
+        <a class="navbar-link" class:active={$location === '/settings'} href="/settings">Settings</a>
+        <a
+          class="navbar-link"
+          class:active={$location.startsWith(`/profile/${$username}`)}
+          href={`/profile/${$username}`}
+        >
+          {$username}
+        </a>
       {:else}
-        <a href="/login">Sign in</a>
-        <a href="/register">Sign up</a>
+        <a class="navbar-link" class:active={$location === '/login'} href="/login">Sign in</a>
+        <a class="navbar-link" class:active={$location === '/register'} href="/register">Sign up</a>
       {/if}
     </nav>
   </div>
