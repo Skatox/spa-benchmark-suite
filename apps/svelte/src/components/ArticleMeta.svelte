734f7b47d0c457f109bf871bdc934cb6d3f5a95f
Miguel Useche <miguel.useche@unet.edu.ve>
2025-11-28 14:34:13 -0500

    Update article preview layout (#14)

--- apps/svelte/src/components/ArticleMeta.svelte
+++ apps/svelte/src/components/ArticleMeta.svelte
@@ -1,27 +1,5 @@
 <script>
-  import { authStore } from '../stores/auth'
-  import { articles as articleApi, profiles as profilesApi } from '../services/api'
-  import { createEventDispatcher } from 'svelte'
-  import { get } from 'svelte/store'
-
   export let article
-  const dispatch = createEventDispatcher()
-
-  async function toggleFavorite() {
-    const currentToken = get(authStore).token
-    if (!currentToken) return dispatch('favorite', article)
-    const method = article.favorited ? articleApi.unfavorite : articleApi.favorite
-    const response = await method(currentToken, article.slug)
-    dispatch('favorite', response.article)
-  }
-
-  async function toggleFollow() {
-    const currentToken = get(authStore).token
-    if (!currentToken) return
-    const method = article.author.following ? profilesApi.unfollow : profilesApi.follow
-    const response = await method(currentToken, article.author.username)
-    article = { ...article, author: response.profile }
-  }
 </script>
 
 <div class="article-meta">
@@ -32,10 +10,4 @@
     <a href={`/profile/${article.author.username}`} class="author">{article.author.username}</a>
     <span class="date">{new Date(article.createdAt).toDateString()}</span>
   </div>
-  <button class="btn small" on:click={toggleFollow}>
-    {article.author.following ? 'Unfollow' : 'Follow'} {article.author.username}
-  </button>
-  <button class="btn small" on:click={toggleFavorite}>
-    ❤ {article.favoritesCount}
-  </button>
 </div>
--- apps/svelte/src/components/ArticlePreview.svelte
+++ apps/svelte/src/components/ArticlePreview.svelte
@@ -1,17 +1,27 @@
 <script>
   import { createEventDispatcher } from 'svelte'
   import ArticleMeta from './ArticleMeta.svelte'
+  import { authStore } from '../stores/auth'
+  import { articles as articleApi } from '../services/api'
+  import { get } from 'svelte/store'
 
   export let article
   const dispatch = createEventDispatcher()
 
-  function onFavorite() {
-    dispatch('favorite', article)
+  async function onFavorite() {
+    const currentToken = get(authStore).token
+    if (!currentToken) return dispatch('favorite', article)
+    const method = article.favorited ? articleApi.unfavorite : articleApi.favorite
+    const response = await method(currentToken, article.slug)
+    dispatch('favorite', response.article)
   }
 </script>
 
 <div class="article-preview">
-  <ArticleMeta {article} on:favorite={onFavorite} />
+  <div class="article-meta-row">
+    <ArticleMeta {article} />
+    <button class="btn small" on:click={onFavorite}>❤ {article.favoritesCount}</button>
+  </div>
   <a href={`/article/${article.slug}`} style="color: inherit;">
     <h2>{article.title}</h2>
     <p>{article.description}</p>
